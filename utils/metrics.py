bb4a6549f6b256c5a33b30f26e5aa140fe2d9d9f
myladivyasaisri <divyasaisri429@gmail.com>
2026-07-22 18:29:34 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -14,12 +14,10 @@ def calculate_mar(mouth):
     """
     Calculates the Mouth Aspect Ratio (MAR) for Yawn Detection
     """
-    # నోటి నిలువు ల్యాండ్‌మార్క్స్ మధ్య దూరం (Vertical distances)
-    A = dist.euclidean(mouth[2], mouth[10]) # టాప్ & బాటమ్ లిప్ పాయింట్స్
+    A = dist.euclidean(mouth[2], mouth[10]) 
     B = dist.euclidean(mouth[4], mouth[8])  
     
-    # నోటి అడ్డు ల్యాండ్‌మార్క్స్ మధ్య దూరం (Horizontal distance)
-    C = dist.euclidean(mouth[0], mouth[6])  # లెఫ్ట్ & రైట్ కార్నర్స్
+    C = dist.euclidean(mouth[0], mouth[6])  
     
     # Compute the Mouth Aspect Ratio
     mar = (A + B) / (2.0 * C)
